3cda02d7f2db7072529e6e5d28d0d32b5847c4f1
Eric <eric.m2hk@gmail.com>
2022-04-17 21:25:56 +0800

    create random users

--- src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
+++ src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
@@ -1,7 +1,14 @@
 package com.hoaxify.hoaxify;
 
+import com.hoaxify.hoaxify.user.User;
+import com.hoaxify.hoaxify.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class HoaxifyApplication {
@@ -10,4 +17,18 @@ public class HoaxifyApplication {
         SpringApplication.run(HoaxifyApplication.class, args);
     }
 
+    @Bean
+    @Profile("!test")
+    CommandLineRunner run(UserService userService) {
+        return args -> {
+            IntStream.rangeClosed(1, 15)
+                    .mapToObj(i -> {
+                        User user = new User();
+                        user.setUsername("user" + i);
+                        user.setDisplayName("display" + i);
+                        user.setPassword("P4ssword");
+                        return user;
+                    }).forEach(userService::save);
+        };
+    }
 }
