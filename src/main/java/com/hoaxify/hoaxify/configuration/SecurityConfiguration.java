6fe116354d46695a3e5d1440938ba988d750f57e
Eric <eric.m2hk@gmail.com>
2022-04-17 17:06:42 +0800

    handle the login error

--- src/main/java/com/hoaxify/hoaxify/configuration/SecurityConfiguration.java
+++ src/main/java/com/hoaxify/hoaxify/configuration/SecurityConfiguration.java
@@ -1,16 +1,19 @@
 package com.hoaxify.hoaxify.configuration;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.httpBasic();
+
+        http.httpBasic().authenticationEntryPoint(new BasicAuthenticationEntryPoint());
 
         http
                 .authorizeHttpRequests().antMatchers(HttpMethod.POST, "/api/1.0/login").authenticated()
--- src/main/java/com/hoaxify/hoaxify/user/LoginController.java
+++ src/main/java/com/hoaxify/hoaxify/user/LoginController.java
@@ -1,17 +1,23 @@
 package com.hoaxify.hoaxify.user;
 
+import com.hoaxify.hoaxify.error.ApiError;
 import com.hoaxify.hoaxify.shared.GenericResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.nio.file.AccessDeniedException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class LoginController {
     @PostMapping("/api/1.0/login")
     void handleLogin() {
-        
-    }
 
+    }
 }
